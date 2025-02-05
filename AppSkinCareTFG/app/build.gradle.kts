a2194d8cad38160aa3a6634031683aa2a46a2cca
Carlos Gómez <carlosgomzp21@gmail.com>
2025-02-05 20:54:32 +0100

    cambios añadidos

--- AppSkinCareTFG/app/build.gradle.kts
+++ AppSkinCareTFG/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "es.upgrade"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 

44fc55ca7df51c48a8f2231dc5739d701538ab15
Carlos Gómez <carlosgomzp21@gmail.com>
2025-02-09 15:32:51 +0100

    Añadida api para servicio rest a traves de JSON Server

---
+++ AppSkinCareTFG/app/src/main/java/es/upgrade/dao/api/ApiService.java
@@ -0,0 +1,12 @@
+package es.upgrade.dao.api;
+
+import java.util.List;
+
+import es.upgrade.entidad.Product;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET("productos")
+    Call<List<Product>> getProductos();
+}
